4c4d867e95c36f0342b58ee80be036a4c9f345ac
Clarissa de Cássia <clari.developer@gmail.com>
2023-04-04 17:01:47 -0300

    alterado JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,19 @@
-//* Pegar todos as perguntas
-
-
-  let pergunta = document.getElementsByClassName('pergunta');
+let pergunta = document.getElementsByClassName('pergunta');
 
  
-
-
-//* Percorrer todos os acordeons quando clicar neles, colocar o painel a amostra e ao clicar novamente 
-//* testar se o painel naquele indice não tiver a classe painel ativo (ou seja com display block), add class
-//* se tiver remover class
-
-
 for (let i = 0; i < pergunta.length; i++) {
     pergunta[i].addEventListener('click', () => {
+
         let respostas = document.getElementsByClassName('resposta')
         let setas = document.getElementsByClassName('seta')
-        if (respostas[i].classList.contains !== ('painel-ativo')) {
-            respostas[i].classList.toggle('resposta-ativa')
-            setas[i].classList.toggle('seta-ativa')
+
+        for (let j = 0; j < respostas.length; j++) {
+            respostas[j].classList.remove('resposta-ativa')
+            setas[j].classList.remove('seta-ativa')
         }
+
+        respostas[i].classList.add('resposta-ativa')
+        setas[i].classList.add('seta-ativa')
     })
 }
 
