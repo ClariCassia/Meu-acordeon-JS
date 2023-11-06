56315281ab01d277f315f54b252900181379bbb2
Clarissa de Cassia <clari.developer@gmail.com>
2023-11-06 18:46:48 -0300

    refatoração código com uso de operador ternário

--- src/js/index.js
+++ src/js/index.js
@@ -5,19 +5,18 @@ perguntas.forEach((item, index) => {
         let respostas = document.querySelectorAll(".resposta");
         let setas = document.querySelectorAll(".seta");
 
-        if (!respostas[index].classList.contains("resposta-ativa")) {
-            respostas.forEach((cadaResposta) => {
-                cadaResposta.classList.remove("resposta-ativa");
-                setas.forEach((setaAtiva) => {
-                    setaAtiva.classList.remove("seta-ativa");
-                })
-            })
-            respostas[index].classList.add("resposta-ativa");
-            setas[index].classList.add("seta-ativa");
+        let campoAtivo = respostas[index].classList.contains("resposta-ativa")
+        console.log("🚀 ~ file: index.js:9 ~ item.addEventListener ~ campoAtivo:", campoAtivo)
 
-        } else {
-            respostas[index].classList.remove("resposta-ativa");
-            setas[index].classList.remove("seta-ativa");
-        }
+        respostas.forEach((cadaResposta) => {
+            cadaResposta.classList.remove("resposta-ativa");
+            setas.forEach((setaAtiva) => {
+                setaAtiva.classList.remove("seta-ativa");
+            })
+        })
+
+        campoAtivo ? (respostas[index].classList.remove('resposta-ativa'), setas[index].classList.remove('seta-ativa'))
+        : (respostas[index].classList.add('resposta-ativa'), setas[index].classList.add('seta-ativa'));
+
     })
 })
